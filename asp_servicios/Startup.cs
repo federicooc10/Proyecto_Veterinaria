9c1d6f50b424fd537aa04e60ef65bc1a09962fd2
Federico <federicoospinacardona3@gmail.com>
2025-05-18 22:07:21 -0500

    termine de organizar la libreria aplicaciones y sigue el servicio

---
+++ asp_servicios/Startup.cs
@@ -0,0 +1,60 @@
+﻿using asp_servicios.Controllers;
+using lib_aplicaciones.Implementaciones;
+using lib_aplicaciones.Interfaces;
+using lib_repositorios.Implementaciones;
+using lib_repositorios.Interfaces;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace asp_servicios
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static IConfiguration? Configuration { set; get; }
+
+        public void ConfigureServices(WebApplicationBuilder builder, IServiceCollection services)
+        {
+            services.Configure<KestrelServerOptions>(x => { x.AllowSynchronousIO = true; });
+            services.Configure<IISServerOptions>(x => { x.AllowSynchronousIO = true; });
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            //services.AddSwaggerGen();
+            // Repositorios
+            services.AddScoped<IConexion, Conexion>();
+            // Aplicaciones
+            services.AddScoped<IClientesAplicacion, ClientesAplicacion>();
+            services.AddScoped<IFormulas_MedicamentosAplicacion, Formulas_MedicamentosAplicacion>();
+            services.AddScoped<IFormulasAplicacion, FormulasAplicacion>();
+            services.AddScoped<IHistoriales_ClinicosAplicacion, Historiales_ClinicosAplicacion>();
+            services.AddScoped<IMascotasAplicacion, MascotasAplicacion>();
+            services.AddScoped<IMedicamentosAplicacion, MedicamentosAplicacion>();
+            services.AddScoped<IRazasAplicacion, RazasAplicacion>();
+            services.AddScoped<IVeterinariosAplicacion, VeterinariosAplicacion>();
+            // Controladores
+            services.AddScoped<TokenController, TokenController>();
+
+            services.AddCors(o => o.AddDefaultPolicy(b => b.AllowAnyOrigin()));
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                //app.UseSwagger();
+                //app.UseSwaggerUI();
+            }
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.UseCors();
+        }
+    }
+}
