ac2c82f4eb02c8edd076181f86f7eed718f478bf
Federico <federicoospinacardona3@gmail.com>
2025-05-18 22:36:40 -0500

    cree el asp de presentaciones y la libreria de presentaciones

---
+++ lib_presentaciones/Interfaces/IClientesPresentacion.cs
@@ -0,0 +1,13 @@
+﻿using lib_dominio.Entidades;
+
+namespace lib_presentaciones.Interfaces
+{
+    public interface IClientesPresentacion
+    {
+        Task<List<Clientes>> Listar();
+        Task<List<Clientes>> PorCodigo(Clientes? entidad);
+        Task<Clientes?> Guardar(Clientes? entidad);
+        Task<Clientes?> Modificar(Clientes? entidad);
+        Task<Clientes?> Borrar(Clientes? entidad);
+    }
+}
